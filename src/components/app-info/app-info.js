964ef95eee1fc3be32eb1f9baba7a6ee4367ff9d
John_Galt <john.galt.prog@gmail.com>
2023-02-08 20:47:34 +0200

    filling components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учёт сотрудников в компании ИА "Информатор"</h1>
+            <h2>Общее кол-во сотрудников: </h2>
+            <h2>Премию получат: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
