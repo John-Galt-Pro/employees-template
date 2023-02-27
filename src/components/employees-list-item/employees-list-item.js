9d86c6279194c26cf7950b44ad174aed8f0c7593
John_Galt <john.galt.prog@gmail.com>
2023-02-27 18:16:35 +0200

    add props in employees-list-item

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,10 +1,20 @@
-import './employees-list-item.css'
+import './employees-list-item.css';
 
-const EmployeesListItem = () => {
+const EmployeesListItem = ({name, salary, increase}) => {
+
+    let classNames = "list-group-item d-flex justify-content-between";
+
+    if(increase) {
+        classNames += ' increase';
+    }
+
     return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Galt</span>
-            <input type="text" className="list-group-item-input" defaultValue="1000$"/>
+        <li className={classNames}>
+            <span className="list-group-item-label">{name}</span>
+
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+
+
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                         className="btn-cookie btn-sm">
@@ -17,8 +27,10 @@ const EmployeesListItem = () => {
                 </button>
                 <i className="fas fa-star"></i>
             </div>
+
         </li>
     )
 }
 
 export default EmployeesListItem;
+
